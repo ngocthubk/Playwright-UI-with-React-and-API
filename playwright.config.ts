c8d86c17d7c95b160b335d0841653765ad071f2a
Administrator <ngocthubk@gmail.com>
2025-08-26 12:16:43 +0200

    Add screenshot on failure and add env.sample

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,11 +35,12 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://practice.expandtesting.com/notes/',
-
+    screenshot: 'only-on-failure',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
