720cd5a00e7f09f410364c769c89d0a907504ded
Thu Nguyen <ngocthubk@gmail.com>
2025-09-03 12:11:03 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.baseURL,
+    baseURL: process.env.baseURL? process.env.baseURL: 'https://practice.expandtesting.com/notes/',
     screenshot: 'only-on-failure',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
